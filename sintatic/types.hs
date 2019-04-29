4d433015ebf9c4810b2bcce75356cc2d2fd5fb39
Gabriela Cavalcante da Silva <gabicavalcantesilva@gmail.com>
2019-04-29 11:45:35 -0300

    fix gitignore

--- sintatic/types.hs
+++ sintatic/types.hs
@@ -1,47 +1,20 @@
-module Types (TokenTree(..), NonTToken(..), parser) where
+module Types (parser) where
 import Text.Parsec
 
 import Tokens
-
-data TokenTree = QuadTree NonTToken TokenTree TokenTree TokenTree TokenTree |
-                 TriTree NonTToken TokenTree TokenTree TokenTree |
-                 DualTree NonTToken TokenTree TokenTree |
-                 UniTree NonTToken TokenTree |
-                 None |
-                 LeafToken Token deriving (Eq, Show)
-
-data NonTToken = 
-    NonTProgram |
-    NonTStatements |
-    NonTStatement |       
-    NonTDecl               |   
-    NonTListType           |
-    NonTStructType deriving (Eq, Show)
-
-makeToken :: Token -> TokenTree
-makeToken tok = LeafToken tok
-
+  
 -- parser to tokens
-typeIntToken :: ParsecT [Token] st IO (Token)
+idToken = tokenPrim show update_pos get_token where
+    get_token (Id pos x)    = Just (Id pos x)
+    get_token _             = Nothing
+ 
 typeIntToken = tokenPrim show update_pos get_token where
-  get_token (TypeInt pos) = Just (TypeInt pos)
-  get_token _             = Nothing
+    get_token (TypeInt pos) = Just (TypeInt pos)
+    get_token _             = Nothing
 
-typeFloatToken :: ParsecT [Token] st IO (Token)
-typeFloatToken = tokenPrim show update_pos get_token where
-  get_token (TypeFloat pos)   = Just (TypeFloat pos)
-  get_token _                 = Nothing
-
-typeStringToken :: ParsecT [Token] st IO (Token)
-typeStringToken = tokenPrim show update_pos get_token where
-  get_token (TypeString pos)    = Just (TypeString pos)
-  get_token _                   = Nothing 
-
-typeBooleanToken :: ParsecT [Token] st IO (Token)
-typeBooleanToken = tokenPrim show update_pos get_token where
-  get_token (TypeBoolean pos) = Just (TypeBoolean pos)
-  get_token _                 = Nothing 
-
+attribToken = tokenPrim show update_pos get_token where
+    get_token (Attrib pos) = Just (Attrib pos)
+    get_token _            = Nothing
 
 update_pos :: SourcePos -> Token -> [Token] -> SourcePos
 update_pos pos _ (tok:_) = pos 
@@ -49,83 +22,30 @@ update_pos pos _ []      = pos
 
 
 -- parsers nao terminais
---         Parsec   input       state         output
-program :: ParsecT [Token] [(Token,Token)] IO(TokenTree)
-program =
-    do c <- stmts
-        eof
-        return (UniTree NonTProgram c)
- 
-
-stmts :: ParsecT [Token] [(Token,Token)] IO(TokenTree)
-stmts = try (
-    do
-        a <- singleStmt
+program :: Parsec [Token] st [Token]
+program = do 
+        a <- idToken 
         b <- stmts
-        return (DualTree NonTStatements a b)
-    ) <|> try (
-    do
-        a <- singleStmt
-        return (UniTree NonTStatement a)
-    )
+        eof
+        return (a:b)
 
-singleStmt :: ParsecT [Token] [(Token,Token)] IO(TokenTree)
-singleStmt =  
-    -- Basico
-    do
-    first <- basicStmt
-    colon <- semicolonToken
-    return first
+stmts :: Parsec [Token] st [Token]
+stmts = do
+        first <- assign
+        return (first)
 
-    
-basicStmt :: ParsecT [Token] [(Token,Token)] IO(TokenTree)
-basicStmt = try (
-    -- Declarações
-    do
-        first <- decl
-        return first
-    ) 
+assign :: Parsec [Token] st [Token]
+assign = do
+        a <- idToken
+        b <- attribToken
+        c <- typeIntToken
+        return (a:b:[c])
 
-decl :: ParsecT [Token] [(Token,Token)] IO(TokenTree)
-decl =  
-    do 
-        type_token <- types
-        id <- listIds
-        return ( DualTree NonTDecl type_token id)  
+parser :: [Token] -> Either ParseError [Token]
+parser tokens = runParser program () "Error message" tokens
  
-    
-
-types :: ParsecT [Token] [(Token,Token)] IO(TokenTree)
-types = try (
-    do
-        t <- openBracketToken
-        t2 <- types
-        t3 <- closeBracketToken
-        return (UniTree NonTListType t2)
-    ) <|> try (
-    do
-        t <- typeIntToken
-        return (LeafToken t)
-    ) <|> try (
-    do 
-        t <- typeFloatToken
-        return (LeafToken t)
-    ) <|> try (
-    do 
-        t <- typeStringToken
-        return (LeafToken t)
-    ) <|> try (
-    do
-        t <- typeBooleanToken
-        return (LeafToken t)
-    ) <|> (
-    do
-        id <- idToken
-        return (UniTree NonTStructType (makeToken id))
-    )
-
-
-
-
-parser :: [Token] -> IO ()
-parser tokens = runParserT program [] "Error message" tokens
+main :: IO ()
+main = case parser (getTokens "1-program.ml") of
+    { Left err -> print err; 
+        Right ans -> print ans
+    }
