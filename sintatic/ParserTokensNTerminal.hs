62ec5843d00692a3236f5bc962f2f401e32c81fa
Gabriela Cavalcante da Silva <gabicavalcantesilva@gmail.com>
2019-06-15 15:04:09 -0300

    Evaluating Expressions

--- sintatic/ParserTokensNTerminal.hs
+++ sintatic/ParserTokensNTerminal.hs
@@ -240,107 +240,12 @@ append_memory :: Variable -> Memory -> Memory
 append_memory variable (Memory []) = Memory [variable]
 append_memory variable (Memory variables) = Memory(variable : variables)
 
--- Evaluating Expressions
---              memory    arvoreExpr   Memory e valor encontrado
-evaluateExpr :: Memory -> ExprTree -> (Memory, (Type, Value))
-evaluateExpr memory exprTree = case exprTree of
-    -- atomics
-    AtomicToken a -> case a of
-        IntLit _ v -> (memory, (IntType, Int v))
-        FloatLit _ v -> (memory, (FloatType, Float v))
-        StrLit _ v -> (memory, (StringType, String v))
-        SymTrue _ -> (memory, (BoolType, Bool True))
-        SymFalse _ -> (memory, (BoolType, Bool False))
-    TriTree a b c -> evaluateTriTree memory a b c
-
-
-evaluateTriTree :: Memory -> ExprTree -> ExprTree -> ExprTree -> (Memory, (Type, Value))
--- Adicao :   a + b
-evaluateTriTree memory (AtomicToken (SymOpPlus _)) a b = res
-    where
-        (mem1, (type1, val1)) = evaluateExpr memory a
-        (mem2, (type2, val2)) = evaluateExpr mem1 b
-        res = (mem2, exprSum (type1, val1) (type2, val2))
-
--- Subtracao :  a - b
-evaluateTriTree mem (AtomicToken (SymOpMinus _)) a b = res
-    where
-        (mem1, (type1, val1)) = evaluateExpr mem a
-        (mem2, (type2, val2)) = evaluateExpr mem1 b
-        res = (mem2, exprMinus (type1, val1) (type2, val2))
-
--- Multiplicacao : a * b
-evaluateTriTree mem (AtomicToken (SymOpMult _)) a b = res
-    where
-        (mem1, (type1, val1)) = evaluateExpr mem a
-        (mem2, (type2, val2)) = evaluateExpr mem1 b
-        res = (mem2, exprMult (type1, val1) (type2, val2))
-
--- Divisao : a / b
-evaluateTriTree mem (AtomicToken (SymOpDiv _)) a b = res
-    where
-        (mem1, (type1, val1)) = evaluateExpr mem a
-        (mem2, (type2, val2)) = evaluateExpr mem1 b
-        res = (mem2, exprDiv (type1, val1) (type2, val2))
-
--- Exponenciacao : a ^ b
-triTreeExprParser mem (AtomicToken (SymOpExp _)) a b = res
-    where
-        (mem1, (type1, val1)) = evaluateExpr mem a
-        (mem2, (type2, val2)) = evaluateExpr mem1 b
-        res = (mem2, exprExp (type1, val1) (type2, val2))
-
-
-intToFloat :: Int -> Float
-intToFloat a = fromInteger (toInteger a)
-
-exprSum :: (Type, Value) -> (Type, Value) -> (Type, Value)
-exprSum (IntType, Int a) (IntType, Int b) = (IntType, Int (a + b))
-exprSum (FloatType, Float a) (FloatType, Float b) = (FloatType, Float (a + b))
-exprSum (IntType, Int a) (FloatType, Float b) = (FloatType, Float ( intToFloat a + b ))
-exprSum (FloatType, Float a) (IntType, Int b) = (FloatType, Float ( a + intToFloat b ))
-exprSum (StringType, String a) (StringType, String b) = (StringType, String (a ++ b))
-exprSum a b = error ("Operação entre os tipos " ++ (show a) ++ " e " ++ (show b) ++ " não é permitida")
-
-exprMinus :: (Type, Value) -> (Type, Value) -> (Type, Value)
-exprMinus (IntType, Int a) (IntType, Int b) = (IntType, Int (a - b))
-exprMinus (FloatType, Float a) (FloatType, Float b) = (FloatType, Float (a - b))
-exprMinus (IntType, Int a) (FloatType, Float b) = (FloatType, Float ( intToFloat a - b ))
-exprMinus (FloatType, Float a) (IntType, Int b) = (FloatType, Float ( a - intToFloat b ))
-exprMinus a b = error ("Operação entre os tipos " ++ (show a) ++ " e " ++ (show b) ++ " não é permitida")
-
-exprMult :: (Type, Value) -> (Type, Value) -> (Type, Value)
-exprMult (IntType, Int a) (IntType, Int b) = (IntType, Int (a * b))
-exprMult (FloatType, Float a) (FloatType, Float b) = (FloatType, Float (a * b))
-exprMult (IntType, Int a) (FloatType, Float b) = (FloatType, Float ( intToFloat a * b ))
-exprMult (FloatType, Float a) (IntType, Int b) = (FloatType, Float ( a * intToFloat b ))
-exprMult a b = error ("Operação entre os tipos " ++ (show a) ++ " e " ++ (show b) ++ " não é permitida")
-
-exprDiv :: (Type, Value) -> (Type, Value) -> (Type, Value)
-exprDiv (IntType, Int a) (IntType, Int b) = (IntType, Int (a `div` b))
-exprDiv (FloatType, Float a) (FloatType, Float b) = (FloatType, Float (a / b))
-exprDiv (IntType, Int a) (FloatType, Float b) = (FloatType, Float ( intToFloat a / b ))
-exprDiv (FloatType, Float a) (IntType, Int b) = (FloatType, Float ( a / intToFloat b ))
-exprDiv a b = error ("Operação entre os tipos " ++ (show a) ++ " e " ++ (show b) ++ " não é permitida")
-
-exprMod :: (Type, Value) -> (Type, Value) -> (Type, Value)
-exprMod (IntType, Int a) (IntType, Int b) = (IntType, Int (a `rem` b))
-exprMod a b = error ("Operação entre os tipos " ++ (show a) ++ " e " ++ (show b) ++ " não é permitida")
-
-exprExp :: (Type, Value) -> (Type, Value) -> (Type, Value)
-exprExp (IntType, Int a) (IntType, Int b) = (IntType, Int (a ^ b))
-exprExp (FloatType, Float a) (FloatType, Float b) = (FloatType, Float (a ** b))
-exprExp (IntType, Int a) (FloatType, Float b) = (FloatType, Float ( intToFloat a ** b ))
-exprExp (FloatType, Float a) (IntType, Int b) = (FloatType, Float ( a ** intToFloat b ))
-exprExp a b = error ("Operação entre os tipos " ++ (show a) ++ " e " ++ (show b) ++ " não é permitida")
-
-
 parser :: [Token] -> IO (Either ParseError ExprTree)
 parser tokens = runParserT program (Memory []) "Error message" tokens
 
-main :: IO ()
-main = case unsafePerformIO (parser (getTokens "problem1.ml")) of
-    {
-        Left err -> print err;
-        Right ans -> print ans
-    }
+--main :: IO ()
+--main = case unsafePerformIO (parser (getTokens "problem1.ml")) of
+--    {
+--        Left err -> print err;
+--        Right ans -> print ans
+--    }
